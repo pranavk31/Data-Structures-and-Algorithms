0246275a039ae53b1f445243171c67842bc035f3
pranavk31 <137434964+pranavk31@users.noreply.github.com>
2026-07-12 23:42:54 +0530

    Create Solution.java

---
+++ 852-Peak-Index-in-a-Mountain-Array/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int peakIndexInMountainArray(int[] arr) {
+        int s = 0, e = arr.length-1;
+        while(s<=e){
+            int mid = s + (e-s)/2;
+
+            if(arr[mid]<arr[mid + 1]){
+                s = mid + 1; 
+            }else if(arr[mid]>arr[mid + 1]){
+                e = mid - 1;
+            }
+            
+        }
+        return s;
+    }
+}
